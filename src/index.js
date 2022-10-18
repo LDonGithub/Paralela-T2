c1af8ae6e062b6b953433c87aabf3f89916ff8c3
LDonGithub <ldonoso@utem.cl>
2022-10-17 23:31:03 -0300

    Creacion y Confg. Api Inicial

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+//settings
+app.set('port', process.env.PORT || 8080);
+app.set('json spaces', 2);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//routes
+app.use(require('./routes/index'));
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
+});
