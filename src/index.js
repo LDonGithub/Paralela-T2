e133ed385b8cacd159c392c0b14c46a55be54a83
Janieck <jeansandocastro@gmail.com>
2022-10-25 18:57:39 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const fetch = require('node-fetch');
+
+//prueba fetch
+/*
+let poke;
+fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=10')
+    .then(res => res.json())
+    .then(data => poke = data);
+*/
+app.get('/pokemon/:id', (req, res) => {
+    let Tag = req.params.id;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${Tag}`)
+    .then(res => res.json())
+    .then(data => poke = data);
+    res.json(poke);
+});
 
 //settings
 app.set('port', process.env.PORT || 8080);
@@ -11,7 +27,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //routes
-app.use(require('./routes/index'));
+app.use(require('./routes/pokemon'));
 
 //starting the server
 app.listen(app.get('port'), () => {
