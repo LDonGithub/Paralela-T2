35b60b88a72bc2ac43a3becf2e2f994276e1f211
Janieck <jeansandocastro@gmail.com>
2022-10-29 01:47:17 -0300

    Fix & Comments

--- API/src/routes/pokemon.js
+++ API/src/routes/pokemon.js
@@ -2,10 +2,12 @@ const {Router} = require('express');
 const router = Router();
 const fetch = require('node-fetch');
 
+//ruta creada para verificar la API
 router.get('/', (req, res) => {
     res.json({"Tittle":"Hello World"});
 });
 
+//ruta creada para paginar los datos de la API
 router.get('/dex/', (req, res) =>{
     fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=905`)
     .then((res) => res.json())
@@ -13,6 +15,7 @@ router.get('/dex/', (req, res) =>{
     //.then((data) => console.log(data));
 });
 
+//ruta creada para buscar PKMN por ID
 router.get('/id/:num', (req, res) =>{
     let num = req.params.num;
     fetch(`https://pokeapi.co/api/v2/pokemon/${num}/`)
--- FRONT/src/js/index.js
+++ FRONT/src/js/index.js
@@ -1,118 +1,123 @@
 const contenendor = document.querySelector("#resultados");
 
-//constructor
+//constructor del objeto PKMN
 function pokemon(id, nombre, altura, peso, tipo, formas, habilidades, ubicacion, imagen) {
-    this.id = id;
-    this.nombre = nombre;
-    this.altura = altura;
-    this.peso = peso;
-    this.tipo = tipo;
-    this.formas = formas;//pendiente
-    this.habilidades = habilidades;
-    this.ubicacion = ubicacion;//pendiente
-    this.imagen = imagen;
+  this.id = id;
+  this.nombre = nombre;
+  this.altura = altura;
+  this.peso = peso;
+  this.tipo = tipo;
+  this.formas = formas; 
+  this.habilidades = habilidades;
+  this.ubicacion = ubicacion; 
+  this.imagen = imagen;
 }
 
-function busqueda(){
-
-    let id = (document.getElementById("tag").value) -1;
-    fetch(`http://localhost:8080/dex/`)
+//Funcion que recibe la ruta de busqueda de la API 
+//Expone resultados en el HTML
+function busqueda() {
+  let id = (document.getElementById("tag").value) - 1;
+  fetch(`http://localhost:8080/dex/`)
     .then((response) => response.json())
     .then((data) => {
-        fetch(data[id].url)
+      fetch(data[id].url)
         .then((res) => res.json())
-        .then((info) => {CrearPokemon(info)})
+        .then((info) => {
+          CrearPokemon(info)
+        })
     })
-};  
-   
+};
 
-async function CrearPokemon(data){
-    
-    pokemonID = new pokemon(data.id,data.name,data.height,data.weight,data.types,data.forms,data.abilities,data.location_area_encounters,data.sprites.front_default);
-    console.log(pokemonID);
-    var card = document.createElement("div");
-    card.setAttribute("class","card mb-5 m-1" );
-    card.setAttribute("style","max-width: 540px;");
-    
-    var ima = document.createElement("img");
-    ima.setAttribute("class","img-fluid rounded-start");
-    ima.setAttribute("src",pokemonID.imagen);
-    ima.setAttribute("alt","Responsive image");
+//Funcion que organiza y muestra los datos de PKMN en el HMTL
+async function CrearPokemon(data) {
 
-    var cardbody = document.createElement("div");
-    cardbody.setAttribute("class","card-body");
+  pokemonID = new pokemon(data.id, data.name, data.height, data.weight, data.types, data.forms, data.abilities, data.location_area_encounters, data.sprites.front_default);
+  //creacion elementos HMTL
+  var card = document.createElement("div");
+  card.setAttribute("class", "card mb-5 m-1");
+  card.setAttribute("style", "max-width: 540px;");
 
-    var nombre = document.createElement("h5");
-    nombre.setAttribute("class","card-title text-capitalize");
-    nombre.innerHTML = pokemonID.nombre;
+  var ima = document.createElement("img");
+  ima.setAttribute("class", "img-fluid rounded-start");
+  ima.setAttribute("src", pokemonID.imagen);
+  ima.setAttribute("alt", "Responsive image");
 
-    var caras = document.createElement("p");
-    caras.setAttribute("class","card-text text-capitalize");
-    caras.innerHTML+= `ID: ${pokemonID.id} <br>`;
-    caras.innerHTML+= `Altura: ${pokemonID.altura/10} m <br>`;
-    caras.innerHTML+= `Peso: ${pokemonID.peso/10} Kg <br>`;
-    caras.innerHTML+= `Tipo: `; 
+  var cardbody = document.createElement("div");
+  cardbody.setAttribute("class", "card-body");
 
-    if(pokemonID.tipo.length > 1){
-        caras.innerHTML+= `${pokemonID.tipo[0].type.name} & ${pokemonID.tipo[1].type.name} <br>`;
-    }else{
-        caras.innerHTML+= `${pokemonID.tipo[0].type.name} <br>`;
-    }
-    //caras.innerHTML+= `Formas: ${pokemonID.formas.length} <br>`;
-    caras.innerHTML+= `Habilidades: <br>`;
-    pokemonID.habilidades.forEach(element => {
-        console.log(element.ability.name)
-        caras.innerHTML+= `${element.ability.name} <br>`;
-    });
-    
+  var nombre = document.createElement("h5");
+  nombre.setAttribute("class", "card-title text-capitalize");
+  nombre.innerHTML = pokemonID.nombre;
 
-    
-    var div1 = document.createElement("div");
-    div1.setAttribute("class","row g-0");
-    
-    var div2 = document.createElement("div");
-    div2.setAttribute("class","col-md-4");
+  var caras = document.createElement("p");
+  caras.setAttribute("class", "card-text text-capitalize");
+  caras.innerHTML += `ID: ${pokemonID.id} <br>`;
+  caras.innerHTML += `Altura: ${pokemonID.altura/10} m <br>`;
+  caras.innerHTML += `Peso: ${pokemonID.peso/10} Kg <br>`;
+  caras.innerHTML += `Tipo: `;
 
-    var div3 = document.createElement("div");
-    div3.setAttribute("class","col-md-8");
+  if (pokemonID.tipo.length > 1) {
+    caras.innerHTML += `${pokemonID.tipo[0].type.name} & ${pokemonID.tipo[1].type.name} <br>`;
+  } else {
+    caras.innerHTML += `${pokemonID.tipo[0].type.name} <br>`;
+  }
+  caras.innerHTML += `Habilidades: <br>`;
+  pokemonID.habilidades.forEach(element => {
+    console.log(element.ability.name)
+    caras.innerHTML += `${element.ability.name} <br>`;
+  });
 
-    card.appendChild(div1);
-    div1.appendChild(div2);
-    div1.appendChild(div3);
-    div2.appendChild(ima);
-    div3.appendChild(cardbody);
-    cardbody.appendChild(nombre)
-    cardbody.appendChild(caras)
-    contenendor.appendChild(card);
+
+
+  var div1 = document.createElement("div");
+  div1.setAttribute("class", "row g-0");
+
+  var div2 = document.createElement("div");
+  div2.setAttribute("class", "col-md-4");
+
+  var div3 = document.createElement("div");
+  div3.setAttribute("class", "col-md-8");
+
+  //Organiza los elementos y muestra en el HTML
+  card.appendChild(div1);
+  div1.appendChild(div2);
+  div1.appendChild(div3);
+  div2.appendChild(ima);
+  div3.appendChild(cardbody);
+  cardbody.appendChild(nombre);
+  cardbody.appendChild(caras);
+  contenendor.innerHTML = card.outerHTML;
 }
 
+//Variable para mostrar PKMN paginados
 let pokemonsList = document.getElementById("pokemons-list");
 let links = document.getElementById("links");
 
+//Contador OFFSET de la paginacion
 cont = 1;
 
-async function paginacion(url){
+//Funcion que recibe la API y controla la paginacion 
+async function paginacion(url) {
   pokemonsList.innerHTML = "";
-  if(url){
-      for(i = cont; i < cont+10; i++){
-        await fetch(url+`${i}`)
+  if (url) {
+    for (i = cont; i < cont + 10; i++) {
+      await fetch(url + `${i}`)
         .then(info => info.json())
         .then(prom => {
-          console.log(prom);
-          console.log(prom.abilities);
-  
+          
           let tipo = "";
           let habilidades = "";
           prom.abilities.forEach(element => {
-              habilidades+= `${element.ability.name} <br>`;
+            habilidades += `${element.ability.name} <br>`;
           });
 
-          if(prom.types.length>1){
-              tipo = `${prom.types[0].type.name} & ${prom.types[1].type.name}`;
-          }else{
-              tipo = `${prom.types[0].type.name}`
+          if (prom.types.length > 1) {
+            tipo = `${prom.types[0].type.name} & ${prom.types[1].type.name}`;
+          } else {
+            tipo = `${prom.types[0].type.name}`
           }
 
+          //Funcion que crea HTML y organiza la paginacion
           pokemonsList.innerHTML += `
           <div class="card mb-3 col-6 " style="max-width: 540px;">
             <div class="row g-0 p-2">
@@ -128,29 +133,29 @@ async function paginacion(url){
             </div>
           </div>
           `;
-          
+
         })
-      }
-      document.createElement
-      //links.innerHTML = (data.previous) ? `<button type="button" class="btn btn-primary m-5 justify-content-center" onclick="updatePokemons('${data.previous}')">Atrás</button>` : "";
+    }
+    document.createElement
+   
   }
 }
 
 paginacion(`http://localhost:8080/id/`);
 
-function siguiente(){
-  cont=cont+10;
+//Funciones que controla los botones de la paginacion
+function siguiente() {
+  cont = cont + 10;
   paginacion(`http://localhost:8080/id/`);
   document.getElementById('anterior').removeAttribute('disabled', 'false');
 }
 
-function anterior(){
-  if(cont>10){
-    cont=cont-10;
+function anterior() {
+  if (cont > 10) {
+    cont = cont - 10;
     paginacion(`http://localhost:8080/id/`);
   }
-  if(cont==1){
+  if (cont == 1) {
     document.getElementById('anterior').setAttribute('disabled', 'true');
   }
 }
-
